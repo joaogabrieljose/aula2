686017b7fe4982d3d655d637826999b1a025739b
João José <joaogjose@outlook.com>
2024-02-01 13:08:03 +0000

    adicionado o total mensal

--- src/salario/Main.java
+++ src/salario/Main.java
@@ -2,14 +2,19 @@ package salario;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Funcionario trabalhador = new Funcionario("João Gabriel José", 3050, 10);
+        Funcionario trabalhador1, trabalhador2;
 
-        System.out.println("Salário: " + trabalhador.salario());
-        System.out.println(trabalhador.toString());
+        trabalhador1 = new Funcionario("João Gabriel José", 3050, 10);
+        System.out.println("Salário: " + trabalhador1.salario());
+        System.out.println(trabalhador1.toString());
 
-        Funcionario trabalhador2 = new Funcionario("Pedro Zua Pereira", 15000, 8);
+        trabalhador2 = new Funcionario("Pedro Zua Pereira", 15000, 8);
         System.out.println("Salário: " + trabalhador2.salario());
         System.out.println(trabalhador2.toString());
+
+       double total =  trabalhador1.salario() + trabalhador2.salario();
+
+        System.out.printf("total éh: "+total);
 
     }
 }
